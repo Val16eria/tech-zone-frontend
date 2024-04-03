4c9e3031cc872e228c03ef08695af5d7051cae44
Val16eria <KStorer61@icloud.com>
2024-04-03 15:02:01 +0300

    a 404 page has been created

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -1,11 +1,25 @@
 import { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@nextui-org/react";
 
 import "./NotFound.scss";
 
 const NotFound: FC = () => {
+	const navigate = useNavigate();
+
 	return (
-		<div>
-			<p>not found</p>
+		<div className="not-found flex-column custom-container">
+			<div>
+				<p className="not-found__title">404</p>
+				<p className="not-found__description">Страницы с таким адресом не существует</p>
+			</div>
+			<Button
+				color="primary"
+				size="md"
+				onClick={() => navigate("/")}
+			>
+				Перейти на главную
+			</Button>
 		</div>
 	);
 };
